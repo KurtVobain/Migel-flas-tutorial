afec515b1ef42775a671ed1bf00df7d5e16ae7fe
evgenii.karpov <one1two215@yandex.ru>
2022-11-25 10:37:49 +0300

    Changed DATABASE_URL due to Heroku Postgres version

--- config.py
+++ config.py
@@ -8,8 +8,9 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace(
+        "postgres://", "postgresql://", 1
+    ) or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
