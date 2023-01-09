4f9919cf70a07b6818e3b6e2c0f995273604d0f0
evgenii.karpov <one1two215@yandex.ru>
2023-01-09 22:01:43 +0300

    Created brand-new model for private messages

--- app/models.py
+++ app/models.py
@@ -35,6 +35,22 @@ class User(UserMixin, db.Model):
         lazy="dynamic",
     )
 
+    message_sent = db.relationship(
+        'Message',
+        foreign_keys='Message.sender_id',
+        backref='author',
+        lazy='dynamic'
+    )
+
+    message_received = db.relationship(
+        'Message',
+        foreign_keys='Message.recipient_id',
+        backref='recipient',
+        lazy='dynamic'
+    )
+
+    last_message_read_time = db.Column(db.DateTime)
+
     def __repr__(self):
         return "<User {}>".format(self.username)
 
@@ -74,6 +90,13 @@ class User(UserMixin, db.Model):
             current_app.config["SECRET_KEY"],
             algorithm="HS256",
         )
+
+    def new_message(self):
+        last_read_time = self.last_message_read_time or datetime(1900, 1, 1)
+
+        return Message.query.filter_by(recipirnt=self).filter(
+            Message.timestamp > last_read_time
+        ).count()
 
     @staticmethod
     def verify_reset_password_token(token):
@@ -152,3 +175,14 @@ class Post(SearchableMixin, db.Model):
 
     def __repr__(self):
         return "<Post {}>".format(self.body)
+
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    sender_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    recipient_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'Message {self.body}'
