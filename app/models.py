87beee4e0f498b2419f2e4a179b73657640add3b
evgenii.karpov <one1two215@yandex.ru>
2023-02-04 11:06:32 +0300

    Task model

--- app/models.py
+++ app/models.py
@@ -4,6 +4,8 @@ from datetime import datetime
 from hashlib import md5
 from time import time
 
+import rq
+import redis
 from flask import current_app
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -27,6 +29,7 @@ class User(UserMixin, db.Model):
     posts = db.relationship("Post", backref="author", lazy="dynamic")
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    task = db.relationship('Task', backref='user', lazy='dynamic')
     followed = db.relationship(
         "User",
         secondary=followers,
@@ -201,3 +204,26 @@ class Notification(db.Model):
 
     def get_data(self):
         return json.loads(str(self.payload_json))
+
+
+class Task(db.Model):
+    id = db.Column(db.String(36), primary_key=True)
+    name = db.Column(db.String(128), index=True)
+    description = db.Column(db.String(128))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    complete = db.Column(db.Boolean, default=False)
+
+    @staticmethod
+    def get_rq_job(self):
+        try:
+            rq_job = rq.job.Job.fetch(self.id, connection=current_app.redis)
+        except (redis.exceptions.RedisError, rq.exceptions.NoSuchJobError):
+            return None
+        return rq_job
+
+    @staticmethod
+    def get_progress(self):
+        job = self.get_rq_job()
+        return job.meta.get('progress', 0) if job is not None else 100
+
+
